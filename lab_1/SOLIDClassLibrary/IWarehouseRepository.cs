33c6d40968963ccbdf6db52e6e2bd76b8d0f3105
VladyslavaV <vt221_vvyu@student.ztu.edu.ua>
2024-03-10 12:44:06 +0200

    lab_1

---
+++ lab_1/SOLIDClassLibrary/IWarehouseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace SOLIDClassLibrary.Warehouse
+{
+    public interface IWarehouseRepository
+    {
+        void AddProduct(Product.Product product);
+        void RemoveProduct(Product.Product product);
+        void UpdateProduct(string productId, Product.Product newProduct);
+        Product.Product? GetProductByIdOrDefault(string id);
+        Product.Product? GetProductByNameOrDefault(string name);
+    }
+}
