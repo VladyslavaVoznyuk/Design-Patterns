461948283c13fa7f0d3f223144e0f0976ee368b9
VladyslavaV <vt221_vvyu@student.ztu.edu.ua>
2024-05-05 13:56:28 +0300

    add DeleteCommand

--- Design-Patterns/lab_3/ConsoleApp1/Composer/Program.cs
+++ Design-Patterns/lab_3/ConsoleApp1/Composer/Program.cs
@@ -1,126 +1,25 @@
-﻿public abstract class LightNode
+﻿
+public interface ICommand
 {
-    public abstract string OuterHtml { get; }
-    public abstract string InnerHtml { get; }
-
-    public abstract void Accept(IVisitor visitor);
+    void Execute();
 }
 
-public class LightElementNode : LightNode
+public class DeleteCommand : ICommand
 {
-    public string TagName { get; set; }
-    public bool IsBlock { get; set; }
-    public bool IsSelfClosing { get; set; }
-    public List<LightNode> Children { get; set; }
-    public List<string> Classes { get; set; }
+    private LightNode _nodeToRemove;
+    private List<LightNode> _parentNodeChildren;
 
-    public LightElementNode(string tagName, bool isBlock, bool isSelfClosing, List<LightNode> children, List<string> classes)
+    public DeleteCommand(LightNode nodeToRemove, List<LightNode> parentNodeChildren)
     {
-        TagName = tagName;
-        IsBlock = isBlock;
-        IsSelfClosing = isSelfClosing;
-        Children = children;
-        Classes = classes;
+        _nodeToRemove = nodeToRemove;
+        _parentNodeChildren = parentNodeChildren;
     }
 
-    public override string OuterHtml
+    public void Execute()
     {
-        get
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.Append($"<{TagName}");
-
-            if (Classes != null && Classes.Count > 0)
-            {
-                sb.Append($" class=\"{string.Join(" ", Classes)}\"");
-            }
-
-            sb.Append(">");
-
-            if (!IsSelfClosing)
-            {
-                sb.Append(InnerHtml);
-                sb.Append($"</{TagName}>");
-            }
-
-            return sb.ToString();
-        }
-    }
-
-    public override string InnerHtml
-    {
-        get
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (var child in Children)
-            {
-                sb.Append(child.OuterHtml);
-            }
-            return sb.ToString();
-        }
-    }
-
-    public override void Accept(IVisitor visitor)
-    {
-        visitor.VisitElementNode(this);
-        foreach (var child in Children)
-        {
-            child.Accept(visitor);
-        }
+        _parentNodeChildren.Remove(_nodeToRemove);
     }
 }
+var deleteCommand = new DeleteCommand(textNode, p.Children);
+deleteCommand.Execute();
 
-public class LightTextNode : LightNode
-{
-    public string Text { get; set; }
-
-    public LightTextNode(string text)
-    {
-        Text = text;
-    }
-
-    public override string OuterHtml => Text;
-    public override string InnerHtml => Text;
-
-    public override void Accept(IVisitor visitor)
-    {
-        visitor.VisitTextNode(this);
-    }
-}
-
-public interface IVisitor
-{
-    void VisitElementNode(LightElementNode elementNode);
-    void VisitTextNode(LightTextNode textNode);
-}
-
-public class DepthFirstIterator : IVisitor
-{
-    public void VisitElementNode(LightElementNode elementNode)
-    {
-        Console.WriteLine($"Visiting element: {elementNode.TagName}");
-    }
-
-    public void VisitTextNode(LightTextNode textNode)
-    {
-        Console.WriteLine($"Visiting text: {textNode.Text}");
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        var div = new LightElementNode("div", isBlock: true, isSelfClosing: false, children: new List<LightNode>(), classes: new List<string>() { "container" });
-        var h1 = new LightElementNode("h1", isBlock: true, isSelfClosing: false, children: new List<LightNode>(), classes: null);
-        var p = new LightElementNode("p", isBlock: true, isSelfClosing: false, children: new List<LightNode>(), classes: null);
-        var textNode = new LightTextNode("Hello, Vladyslava!");
-
-        div.Children.Add(h1);
-        div.Children.Add(p);
-        h1.Children.Add(new LightTextNode("My Title"));
-        p.Children.Add(textNode);
-        var iterator = new DepthFirstIterator();
-        div.Accept(iterator);
-    }
-}
