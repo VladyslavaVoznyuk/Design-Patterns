a11c25ad9ce214d19568a84195602541689012f9
VladyslavaV <vt221_vvyu@student.ztu.edu.ua>
2024-05-05 14:05:06 +0300

    The State template has been extended with the addition of new states and logic

--- Design-Patterns/lab_3/ConsoleApp1/Composer/Program.cs
+++ Design-Patterns/lab_3/ConsoleApp1/Composer/Program.cs
@@ -1,25 +1,157 @@
-﻿
-public interface ICommand
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public interface IElementState
 {
-    void Execute();
+    void Handle(LightElementNode element);
 }
 
-public class DeleteCommand : ICommand
+public class RegularState : IElementState
 {
-    private LightNode _nodeToRemove;
-    private List<LightNode> _parentNodeChildren;
-
-    public DeleteCommand(LightNode nodeToRemove, List<LightNode> parentNodeChildren)
+    public void Handle(LightElementNode element)
     {
-        _nodeToRemove = nodeToRemove;
-        _parentNodeChildren = parentNodeChildren;
-    }
-
-    public void Execute()
-    {
-        _parentNodeChildren.Remove(_nodeToRemove);
+        Console.WriteLine($"Element {element.TagName} is in regular state.");
     }
 }
-var deleteCommand = new DeleteCommand(textNode, p.Children);
-deleteCommand.Execute();
 
+public class DisabledState : IElementState
+{
+    public void Handle(LightElementNode element)
+    {
+        Console.WriteLine($"Element {element.TagName} is disabled.");
+    }
+}
+
+public class HoveredState : IElementState
+{
+    public void Handle(LightElementNode element)
+    {
+        Console.WriteLine($"Element {element.TagName} is hovered.");
+    }
+}
+
+public abstract class LightNode
+{
+    public abstract string OuterHtml { get; }
+    public abstract string InnerHtml { get; }
+}
+
+public class LightElementNode : LightNode
+{
+    public string TagName { get; set; }
+    public bool IsBlock { get; set; }
+    public bool IsSelfClosing { get; set; }
+    public List<LightNode> Children { get; set; }
+    public List<string> Classes { get; set; }
+    public IElementState State { get; set; }  
+
+    public LightElementNode(string tagName, bool isBlock, bool isSelfClosing, List<LightNode> children, List<string> classes)
+    {
+        TagName = tagName;
+        IsBlock = isBlock;
+        IsSelfClosing = isSelfClosing;
+        Children = children;
+        Classes = classes;
+
+        State = new RegularState();
+    }
+
+    public void ChangeState(IElementState state)
+    {
+        State = state;
+    }
+
+    public void Disable()
+    {
+        ChangeState(new DisabledState());
+    }
+
+    public void Enable()
+    {
+        ChangeState(new RegularState()); 
+    }
+
+    public void Hover()
+    {
+        ChangeState(new HoveredState());
+    }
+
+    public override string OuterHtml
+    {
+        get
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"<{TagName}");
+
+            if (Classes != null && Classes.Count > 0)
+            {
+                sb.Append($" class=\"{string.Join(" ", Classes)}\"");
+            }
+
+            sb.Append(">");
+
+            if (!IsSelfClosing)
+            {
+                sb.Append(InnerHtml);
+                sb.Append($"</{TagName}>");
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    public override string InnerHtml
+    {
+        get
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var child in Children)
+            {
+                sb.Append(child.OuterHtml);
+            }
+            return sb.ToString();
+        }
+    }
+}
+
+public class LightTextNode : LightNode
+{
+    public string Text { get; set; }
+
+    public LightTextNode(string text)
+    {
+        Text = text;
+    }
+
+    public override string OuterHtml => Text;
+    public override string InnerHtml => Text;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var div = new LightElementNode("div", isBlock: true, isSelfClosing: false, children: new List<LightNode>(), classes: new List<string>() { "container" });
+        var h1 = new LightElementNode("h1", isBlock: true, isSelfClosing: false, children: new List<LightNode>(), classes: null);
+        var p = new LightElementNode("p", isBlock: true, isSelfClosing: false, children: new List<LightNode>(), classes: null);
+        var textNode = new LightTextNode("Hello, Vladyslava!");
+
+        div.Children.Add(h1);
+        div.Children.Add(p);
+        h1.Children.Add(new LightTextNode("My Title"));
+        p.Children.Add(textNode);
+
+        Console.WriteLine(div.OuterHtml);
+
+        div.Disable();
+        Console.WriteLine(div.OuterHtml);
+
+        div.Hover();
+        Console.WriteLine(div.OuterHtml);
+
+        div.Enable();
+        Console.WriteLine(div.OuterHtml);
+    }
+}
