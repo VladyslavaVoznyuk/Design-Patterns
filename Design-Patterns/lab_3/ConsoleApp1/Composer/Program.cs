92fc3e56c368f0f16c9824ec22bf2e8463592b38
VladyslavaV <vt221_vvyu@student.ztu.edu.ua>
2024-05-05 14:13:09 +0300

    Implement visitor pattern for HTML rendering

--- Design-Patterns/lab_3/ConsoleApp1/Composer/Program.cs
+++ Design-Patterns/lab_3/ConsoleApp1/Composer/Program.cs
@@ -2,39 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 
-public interface IElementState
+public interface IVisitor
 {
-    void Handle(LightElementNode element);
-}
-
-public class RegularState : IElementState
-{
-    public void Handle(LightElementNode element)
-    {
-        Console.WriteLine($"Element {element.TagName} is in regular state.");
-    }
-}
-
-public class DisabledState : IElementState
-{
-    public void Handle(LightElementNode element)
-    {
-        Console.WriteLine($"Element {element.TagName} is disabled.");
-    }
-}
-
-public class HoveredState : IElementState
-{
-    public void Handle(LightElementNode element)
-    {
-        Console.WriteLine($"Element {element.TagName} is hovered.");
-    }
+    void Visit(LightElementNode node);
+    void Visit(LightTextNode node);
 }
 
 public abstract class LightNode
 {
     public abstract string OuterHtml { get; }
     public abstract string InnerHtml { get; }
+
+    public abstract void Accept(IVisitor visitor);
 }
 
 public class LightElementNode : LightNode
@@ -44,7 +23,6 @@ public class LightElementNode : LightNode
     public bool IsSelfClosing { get; set; }
     public List<LightNode> Children { get; set; }
     public List<string> Classes { get; set; }
-    public IElementState State { get; set; }  
 
     public LightElementNode(string tagName, bool isBlock, bool isSelfClosing, List<LightNode> children, List<string> classes)
     {
@@ -53,28 +31,6 @@ public class LightElementNode : LightNode
         IsSelfClosing = isSelfClosing;
         Children = children;
         Classes = classes;
-
-        State = new RegularState();
-    }
-
-    public void ChangeState(IElementState state)
-    {
-        State = state;
-    }
-
-    public void Disable()
-    {
-        ChangeState(new DisabledState());
-    }
-
-    public void Enable()
-    {
-        ChangeState(new RegularState()); 
-    }
-
-    public void Hover()
-    {
-        ChangeState(new HoveredState());
     }
 
     public override string OuterHtml
@@ -113,6 +69,15 @@ public class LightElementNode : LightNode
             return sb.ToString();
         }
     }
+
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.Visit(this);
+        foreach (var child in Children)
+        {
+            child.Accept(visitor);
+        }
+    }
 }
 
 public class LightTextNode : LightNode
@@ -126,13 +91,37 @@ public class LightTextNode : LightNode
 
     public override string OuterHtml => Text;
     public override string InnerHtml => Text;
+
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
+
+public class HtmlRenderingVisitor : IVisitor
+{
+    public StringBuilder Result { get; }
+
+    public HtmlRenderingVisitor()
+    {
+        Result = new StringBuilder();
+    }
+
+    public void Visit(LightElementNode node)
+    {
+        Result.Append(node.OuterHtml);
+    }
+
+    public void Visit(LightTextNode node)
+    {
+        Result.Append(node.OuterHtml);
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-
         var div = new LightElementNode("div", isBlock: true, isSelfClosing: false, children: new List<LightNode>(), classes: new List<string>() { "container" });
         var h1 = new LightElementNode("h1", isBlock: true, isSelfClosing: false, children: new List<LightNode>(), classes: null);
         var p = new LightElementNode("p", isBlock: true, isSelfClosing: false, children: new List<LightNode>(), classes: null);
@@ -143,15 +132,9 @@ class Program
         h1.Children.Add(new LightTextNode("My Title"));
         p.Children.Add(textNode);
 
-        Console.WriteLine(div.OuterHtml);
+        var visitor = new HtmlRenderingVisitor();
+        div.Accept(visitor);
 
-        div.Disable();
-        Console.WriteLine(div.OuterHtml);
-
-        div.Hover();
-        Console.WriteLine(div.OuterHtml);
-
-        div.Enable();
-        Console.WriteLine(div.OuterHtml);
+        Console.WriteLine(visitor.Result.ToString());
     }
 }
